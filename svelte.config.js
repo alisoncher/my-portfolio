8216850e90a1793027a020123db8d37f132ae403
alisoncher <90787820+alisoncher@users.noreply.github.com>
2024-10-20 22:24:45 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,14 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ fallback: '404.html' }),
+		adapter: adapter({
+			fallback: '404.html', // ensures routing works
+		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			base: '/your-repo-name', // Replace with your actual repository name
 		},
 	},
 };
 
 export default config;
+
