2eda4a1c492f971eb7e9b8c3f3752f6a5c8d30d0
alisoncher <90787820+alisoncher@users.noreply.github.com>
2024-10-20 22:37:54 -0700

    step3

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,11 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html', // ensures routing works
-		}),
+		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: '/my-portfolio', // Replace with your actual repository name
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 		},
 	},
 };
 
 export default config;
-
