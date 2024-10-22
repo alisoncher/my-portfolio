4f7bcdd2eed696ae988573370f0e900a6bc68846
alisoncher <90787820+alisoncher@users.noreply.github.com>
2024-10-21 22:12:29 -0700

    lab4 step3
    nav bar updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,62 @@
 <svelte:head>
   <title>Home</title>
 </svelte:head>
-<nav>
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="projects/">Projects</a></li>
-    <li><a href="contact/">Contact</a></li>
-    <li><a href="resume/">Resume</a></li>
-    <li><a href="https://github.com/alisoncher" target="_blank">GitHub</a></li>
-      
-  
-  <!-- <li><a href="/Users/alisoncher/Documents/GitHub/portfolio/index.html" >Home</a></li>
-  <li><a href="/Users/alisoncher/Documents/GitHub/portfolio/projects/index.html">Projects</a></li>
-  <li><a href="/Users/alisoncher/Documents/GitHub/portfolio/contact/index.html">Contact</a></li>
-  <li><a href="/Users/alisoncher/Documents/GitHub/portfolio/resume/index.html">Resume</a></li>
-  <li><a href="https://github.com/alisoncher" target="_blank">GitHub</a> -->
-  </ul>
 
-  <form id="color-scheme-form">
-      <label for="color-scheme-select">Theme:</label>
-          <select id="color-scheme-select" name="color-scheme">
-              <option value="auto" selected>Automatic (Default)</option>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-          </select>
-  </form>
-  
+<script>
+  let pages = [
+    { url: '/', title: 'Home' },
+    { url: '/projects', title: 'Projects' },
+    { url: '/contact', title: 'Contact' },
+    { url: '/resume', title: 'Resume' },
+  ];
+
+  let currentPath = window.location.pathname;
+</script>
+
+<nav>
+  {#each pages as p}
+    <a href={p.url}
+       class="{p.url === currentPath ? 'current' : ''}">
+      {p.title}
+    </a>
+  {/each}
+  <a href="https://github.com/alisoncher" target="_blank">GitHub</a>
 </nav>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: center;
+    border-bottom: 1px solid #ccc;
+  }
+  
+  nav a {
+    padding: 10px;
+    text-decoration: none;
+    color: inherit;
+    text-align: center;
+  }
+
+  nav a.current {
+    border-bottom: 4px solid #ccc;
+    font-weight: bold;
+  }
+
+  nav a:hover {
+    background-color: #f0f0f0;
+  }
+</style>
+
+<form id="color-scheme-form">
+  <label for="color-scheme-select">Theme:</label>
+      <select id="color-scheme-select" name="color-scheme">
+          <option value="auto" selected>Automatic (Default)</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+      </select>
+</form>
+
+
 <h1>Alison Cher</h1>
 <p>I'm just a girl</p>
 <img src="images/alison.jpg" alt="A cloudy day in New York City" style="width: 300px; height: auto;">
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,21 +2,52 @@
 <svelte:head>
   <title>Projects</title>
 </svelte:head>  
-<nav>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="projects/">Projects</a></li>
-        <li><a href="contact/">Contact</a></li>
-        <li><a href="resume/">Resume</a></li>
-        <li><a href="https://github.com/alisoncher" target="_blank">GitHub</a></li>
-    </ul>
-        
-        <!-- <li><a href="/Users/alisoncher/Documents/GitHub/portfolio/index.html">Home</a></li> 
-        <li><a href="index.html">Projects</a></li> 
-        <li><a href="/Users/alisoncher/Documents/GitHub/portfolio/contact/index.html">Contact</a></li> 
-        <li><a href="/Users/alisoncher/Documents/GitHub/portfolio/resume/index.html">Resume</a></li>
-        <li><a href="https://github.com/alisoncher" target="_blank">GitHub</a></li>  -->
 
+<script>
+    let pages = [
+      { url: '/', title: 'Home' },
+      { url: '/projects', title: 'Projects' },
+      { url: '/contact', title: 'Contact' },
+      { url: '/resume', title: 'Resume' },
+    ];
+  
+    let currentPath = window.location.pathname;
+  </script>
+  
+  <nav>
+    {#each pages as p}
+      <a href={p.url}
+         class="{p.url === currentPath ? 'current' : ''}">
+        {p.title}
+      </a>
+    {/each}
+    <a href="https://github.com/alisoncher" target="_blank">GitHub</a>
+  </nav>
+  
+  <style>
+    nav {
+      display: flex;
+      justify-content: center;
+      border-bottom: 1px solid #ccc;
+    }
+    
+    nav a {
+      padding: 10px;
+      text-decoration: none;
+      color: inherit;
+      text-align: center;
+    }
+  
+    nav a.current {
+      border-bottom: 4px solid #ccc;
+      font-weight: bold;
+    }
+  
+    nav a:hover {
+      background-color: #f0f0f0;
+    }
+  </style>
+  
         <form id="color-scheme-form">
             <label for="color-scheme-select">Theme:</label>
                 <select id="color-scheme-select" name="color-scheme">
@@ -26,7 +57,7 @@
                 </select>
         </form>
     
-</nav>
+
 <h1>Projects</h1>
 
 <div class="projects">
